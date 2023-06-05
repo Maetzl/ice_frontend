4505b1d6f9a36deedc72d07311c514fc66f783a9
Matthias Wohland <wohland.matthias@gmail.com>
2023-06-05 17:45:44 +0200

    test coverage

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 
-test("renders learn react link", () => {
+test("Some Test Test", () => {
   render(<App />);
   const linkElement = screen.getByText(/Login/i);
   expect(linkElement).toBeInTheDocument();
