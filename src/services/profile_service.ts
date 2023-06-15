1ec21bde3cbd1de7c0427b2a89df8a65732f35a4
Matthias Wohland <wohland.matthias@gmail.com>
2023-06-16 01:32:22 +0200

    getProfile as POST with formdata example

--- src/services/profile_service.ts
+++ src/services/profile_service.ts
@@ -5,18 +5,27 @@ const apiServerUrl = process.env.REACT_APP_API_SERVER_URL;
 export const getProfile = async (accessToken: string, userName: any) => {
 
     console.log(userName)
+
+    var form = new FormData();
+
+    form.append("Name", userName);
+    form.append("Test2", "Test2");
+    form.append("Test3", "Test3");
+
     const config = {
       url: `${apiServerUrl}/api/profile/`,
-      method: "GET",
+      method: "POST",
       headers: {
         "content-type": "application/json",
-        Authorization: `Bearer ${accessToken}`,
-        UserName: `Name ${userName}`,
+        Authorization: `Bearer ${accessToken}`
       },
+      data : form
     };
   
     const { data, error } = await callExternalApi({ config });
   
+    console.log(data)
+
     return {
       data: data || null,
       error,
