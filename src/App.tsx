36098d7517f211b7bbb1338c8cacb0607905b86c
Matthias Wohland <wohland.matthias@gmail.com>
2023-06-15 21:14:29 +0200

    Added Auth0

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navigation from "./components/Navigation";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Store from "./pages/Store";
 import Index from "./pages/Index";
-import Library from "./pages/Library";
+import { AuthenticationGuard } from "./components/Authentication_Guard";
 import Profile from "./pages/Profile";
+import Store from "./pages/Store";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import Library from "./pages/Library";
 import PublishYourGames from "./pages/PublishYourGames";
 import Contact from "./pages/Contact";
 import Placeholder from "./pages/Placeholder";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="bg-black md:bg-white">
-      <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="/library" element={<Library />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/publish" element={<PublishYourGames />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/placeholder" element={<Placeholder />} />
-        </Routes>
-        <Footer></Footer>
-      </BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/store" element={<Store />} />
+        <Route
+          path="/library"
+          element={<AuthenticationGuard component={Library} />}
+        />
+        <Route
+          path="/profile"
+          element={<AuthenticationGuard component={Profile} />}
+        />
+        <Route path="/publish" element={<PublishYourGames />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/placeholder" element={<Placeholder />} />
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
+
+console.log(process.env.REACT_APP_AUTH0_DOMAIN);
 
 export default App;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,46 @@
-import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect, useState } from "react";
+import { getProfile } from "../services/profile_service";
 
 export default function Profile() {
-  return <div>Profile</div>;
+  const [message, setMessage] = useState("");
+
+  const { user, getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const getMessage = async () => {
+      const accessToken = await getAccessTokenSilently();
+      const userName = user?.name;
+      const { data, error } = await getProfile(accessToken, userName);
+
+      if (!isMounted) {
+        return;
+      }
+
+      if (data) {
+        setMessage(JSON.stringify(data, null, 2));
+      }
+
+      if (error) {
+        setMessage(JSON.stringify(error, null, 2));
+      }
+    };
+
+    getMessage();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [getAccessTokenSilently]);
+
+  console.log(message);
+
+  return (
+    <div>
+      {user?.sub}
+      {message}
+    </div>
+  );
 }
