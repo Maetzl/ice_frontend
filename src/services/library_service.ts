3e8e2c5f73274e85a40539ebde6b624d25d8c192
sebastianblankenhorn <sebastian.blankenhorn@gmail.com>
2023-06-21 12:44:08 +0200

    fix library service

--- src/services/library_service.ts
+++ src/services/library_service.ts
@@ -2,16 +2,18 @@ import { callExternalApi } from "./external_api_service";
 
 const apiServerUrl = process.env.REACT_APP_API_SERVER_URL;
 
-const getLibrary = async (accessToken: string) => {
+const getLibrary = async (accessToken: string, userData: FormData) => {
   const config = {
     url: `${apiServerUrl}/api/library/`,
-    method: "GET",
+    method: "POST",
     headers: {
       "content-type": "application/json",
       Authorization: `Bearer ${accessToken}`
     },
+    data : userData
   };
   const { data, error } = await callExternalApi({ config });
+  console.log("Library data:", data)
   return {
     data: data || null,
     error,
